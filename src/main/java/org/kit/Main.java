37ffffb59d05f71d7f3ab53585ec9e4b032e7a49
KitEA <kit.earf@gmail.com>
2025-07-04 00:59:17 +0400

    combine tasks into one entry point

--- src/main/java/org/kit/Main.java
+++ src/main/java/org/kit/Main.java
@@ -2,65 +2,31 @@ package org.kit;
 
 import java.util.Scanner;
 
-public class Main {
-    private final static int GREETING_THRESHOLD = 7;
-    private final static String THE_ONLY_NAME = "John";
+import static org.kit.ProcessNumberNameArray.solveProcessNumberNameArray;
+import static org.kit.SequenceMirroring.solveSequenceMirroring;
 
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose what task you want to solve:");
+        System.out.println("1. Process number, name, and numeric array.");
+        System.out.println("2. Check if a sequence is perfectly mirrored.");
 
-        processNumberInput(scanner);
-        processStringInput(scanner);
-        processIntegerArray(scanner);
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                solveProcessNumberNameArray(scanner);
+                break;
+            case 2:
+                solveSequenceMirroring(scanner);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
 
         scanner.close();
     }
-
-    private static void processNumberInput(Scanner scanner) {
-        System.out.println("Enter a number:");
-        try {
-            int number = scanner.nextInt();
-            if (number > GREETING_THRESHOLD) {
-                System.out.println("Hello");
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid number entered.");
-        }
-    }
-
-    private static void processStringInput(Scanner scanner) {
-        System.out.println("Enter a name:");
-        scanner.nextLine();
-        String name = scanner.nextLine();
-        if (THE_ONLY_NAME.equalsIgnoreCase(name)) {
-            System.out.printf("Hello, %s \n", THE_ONLY_NAME);
-        } else {
-            System.out.println("There is no such name");
-        }
-    }
-
-    private static void processIntegerArray(Scanner scanner) {
-        System.out.println("Enter a numeric array separated by commas:");
-        String arrayInput = scanner.nextLine();
-        String[] arrayElements = arrayInput.split(",");
-        for (String element : arrayElements) {
-            try {
-                printIfMultipleOfThree(element);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid number in array: " + element);
-            }
-        }
-    }
-
-    private static void printIfMultipleOfThree(String element) {
-        int num = Integer.parseInt(element.trim());
-        if (multipleOfThree(num)) {
-            System.out.println(num);
-        }
-    }
-
-    private static boolean multipleOfThree(int num) {
-        return num % 3 == 0;
-    }
-
 }
---
+++ src/main/java/org/kit/ProcessNumberNameArray.java
@@ -0,0 +1,61 @@
+package org.kit;
+
+import java.util.Scanner;
+
+public class ProcessNumberNameArray {
+    private final static int GREETING_THRESHOLD = 7;
+    private final static String THE_ONLY_NAME = "John";
+
+    public static void solveProcessNumberNameArray(Scanner scanner) {
+        processNumberInput(scanner);
+        processStringInput(scanner);
+        processIntegerArray(scanner);
+    }
+
+    private static void processNumberInput(Scanner scanner) {
+        System.out.println("Enter a number:");
+        try {
+            int number = scanner.nextInt();
+            if (number > GREETING_THRESHOLD) {
+                System.out.println("Hello");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid number entered.");
+        }
+    }
+
+    private static void processStringInput(Scanner scanner) {
+        System.out.println("Enter a name:");
+        scanner.nextLine();
+        String name = scanner.nextLine();
+        if (THE_ONLY_NAME.equalsIgnoreCase(name)) {
+            System.out.printf("Hello, %s \n", THE_ONLY_NAME);
+        } else {
+            System.out.println("There is no such name");
+        }
+    }
+
+    private static void processIntegerArray(Scanner scanner) {
+        System.out.println("Enter a numeric array separated by commas:");
+        String arrayInput = scanner.nextLine();
+        String[] arrayElements = arrayInput.split(",");
+        for (String element : arrayElements) {
+            try {
+                printIfMultipleOfThree(element);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number in array: " + element);
+            }
+        }
+    }
+
+    private static void printIfMultipleOfThree(String element) {
+        int num = Integer.parseInt(element.trim());
+        if (multipleOfThree(num)) {
+            System.out.println(num);
+        }
+    }
+
+    private static boolean multipleOfThree(int num) {
+        return num % 3 == 0;
+    }
+}
